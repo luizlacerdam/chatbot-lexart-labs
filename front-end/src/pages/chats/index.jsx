c77d593e2175a488565d047e8895dfefb1752e54
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-08-30 09:46:38 -0300

    :sparkles: cd

--- front-end/src/pages/chats/index.jsx
+++ front-end/src/pages/chats/index.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
+import { CSVLink } from 'react-csv';
 import Navbar from '../components/Navbar';
 import { requestData } from '../../utils/requests';
 
 export default function Chats() {
   const [chats, setChats] = React.useState([]);
+  const [filteredChats, setFilteredChats] = React.useState([]);
   const [isLoaded, setIsLoaded] = React.useState(false);
+
   const getAllChats = async () => {
     try {
       const response = await requestData('/');
       setChats(response.allChats);
+      console.log(response.allChats);
       setIsLoaded(true);
     } catch (error) {
       console.log(error);
@@ -28,31 +32,58 @@ export default function Chats() {
     return `${day}/${month}/${year} ${hours}:${minutes}`;
   };
 
+  const csvAllChats = (data) => {
+    const newData = [];
+    data.forEach((chat) => {
+      newData.push({
+        id: chat.id,
+        username: chat.username,
+        finished: formatDate(chat.finished),
+      });
+    });
+    return newData;
+  };
+
+  const csvOneChat = (data) => {
+    const newData = [];
+    newData.push({
+      id: data.id,
+      username: data.username,
+      finished: formatDate(data.finished),
+    });
+    return newData;
+  };
+
   return (
     <div id="phone-wrapper">
-
       <div id="app">
         <Navbar />
-        <h1>Download all</h1>
+        {!isLoaded ? <p>Loading...</p> : (
 
+          <CSVLink data={ csvAllChats(chats) }>
+            <h1>Download all</h1>
+          </CSVLink>
+        )}
         <div id="chats-page">
           <h3>Chats:</h3>
           { !isLoaded ? <p>Loading...</p> : (
             <div id="chats">
               { chats.map((chat) => (
-                <div
+                <CSVLink
                   key={ chat.id }
-                  className="chat"
+                  data={ csvOneChat(chat) }
                 >
-                  <div className="chat-info">
-                    <p>
-                      {`User #${chat.id}
-                       - ${chat.username} - ${formatDate(chat.finished)} `}
+                  <div className="chat">
+                    <div className="chat-info">
+                      <p>
+                        {`User #${chat.id}
+                         - ${chat.username} - ${formatDate(chat.finished)} `}
 
-                    </p>
+                      </p>
+                    </div>
                   </div>
-                  {/* { chat.messages } */}
-                </div>
+                </CSVLink>
+
               )) }
             </div>
           ) }
