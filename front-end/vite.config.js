9e1c282cd6f9631814803b1c43be17a6e79d2e0f
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-08-31 14:47:56 -0300

    exposing

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: { port: 80 },
+  server: { port: 3000 },
   build: { outDir: './build' },
 });
